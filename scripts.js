2efa2c2eba829d057eb1b15d2d24f568e2278acf
oliatom <137240186+oliatom@users.noreply.github.com>
2023-06-26 23:00:49 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+/* JavaScript para verificar la disponibilidad */
+function verificarDisponibilidad() {
+    var codigoPostal = document.getElementById("codigoPostal").value;
+    
+    // Aquí puedes agregar la lógica para verificar la disponibilidad en el código postal ingresado
+    
+    alert("¡La disponibilidad en tu área ha sido verificada! Contáctanos para obtener más información.");
+  }
+  
